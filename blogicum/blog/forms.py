5314ee75e5814ff3e410750fdfaa516506a05412
Pavel Soskov <lingerov@bk.ru>
2025-02-28 21:33:57 +0300

    Убрал коммент

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -7,10 +7,6 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         exclude = ('author',)
-        # Так она же автоматом и встает, формат тут не влияет же.
-        # После первого ревью не вставала,
-        # потому что я забыл убрать не нужную константну из настроек,
-        # эту - DATETIME_INPUT_FORMATS = ['%d-%Y-%m %H:%M'].
         widgets = {
             'pub_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
